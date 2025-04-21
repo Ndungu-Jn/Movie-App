97d9676dd8a1439e6c760701287d5b20aaeec341
John Ndungu <ndungulink@gmail.com>
2025-04-21 10:03:04 +0300

    Installing and setting up tailwinds

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,40 +1,9 @@
-import { use, useEffect, useState, useTransition } from "react";
-
-const Card = ({ title }) => {
-  const [count, setCount] = useState(0);
-  const [hasLiked, setHasLiked] = useState(false);
-
-  useEffect(() => {
-    console.log(`${title} has been ${hasLiked}`);
-  }, [hasLiked]);
-
-  return (
-    <div
-      className="card"
-      onClick={() => setCount((prevState) => prevState + 1)}
-    >
-      <h2>
-        {title} <br /> {count}
-      </h2>
-
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? "❤️" : "🤍"}
-      </button>
-    </div>
-  );
-};
+import React from "react";
 
 const App = () => {
   return (
-    <div className="card-container">
-      <Card
-        title="starwars"
-        rating={5}
-        isCool={true}
-        actors={[{ name: "actors" }]}
-      />
-      <Card title="Avatar" />
-      <Card title="Lion king" />
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 };
